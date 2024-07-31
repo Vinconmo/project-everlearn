cb43b85574111912a683486d6cb778a56aee0df7
Vincent Moser <dinda.mos@t-online.de>
2024-07-31 11:05:00 +0200

    chore: add origin array for cors

--- server/index.js
+++ server/index.js
@@ -9,12 +9,29 @@ const app = new Koa();
 const SERVER_PORT = process.env.SERVER_PORT || 3001;
 const CLIENT_PORT = 5173;
 
+const validOrigins = [
+  `http://localhost:${CLIENT_PORT}`,
+  `http://127.0.0.1:${CLIENT_PORT}`
+]
+
+// Origin verification generator
+function verifyOrigin (ctx) {
+    // Get requesting origin hostname
+    const origin = ctx.headers.origin;
+
+    // Make sure it's a valid origin
+    if (validOrigins.indexOf(origin) != -1) {
+       // Set the header to the requested origin
+      ctx.set('Access-Control-Allow-Origin', origin);
+      return origin
+    }
+}
+
 const corsConfig = {
-  origin: [
-    `http://localhost:${CLIENT_PORT}`],
+  origin: verifyOrigin,
 };
 
-
+// Configure Koa to use kcors module with origin verification
 app.use(cors(corsConfig))
   .use(bodyParser())
   .use(router.routes());
