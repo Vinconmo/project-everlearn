2565a0156fe8a1c3b74bf6259e4a666904488a43
Vincent Moser <dinda.mos@t-online.de>
2024-08-29 21:59:10 +0200

    chore: cleaned env

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const router = require("./router");
 const app = new Koa();
 const config = require("./config.js");
 
-const validOrigins = [...config.CLIENT_URLS];
+const validOrigins = [...config.CLIENT_URLS, "https://main--gleeful-cranachan-4fd23d.netlify.app"];
 
 // Origin verification generator
 function verifyOrigin(ctx) {
@@ -33,6 +33,7 @@ app.use(cors(corsConfig)).use(bodyParser()).use(router.routes());
 (async () => {
   console.log("⏳ Starting the server");
   console.log("🐳 Environment: ", config.ENV);
+  console.log(config.CLIENT_URLS)
   try {
     await db.sequelize.authenticate();
     console.log("💾 Connected to the database!");
